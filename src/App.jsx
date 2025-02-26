88a9ab5221ed633f31c783919687201584fa5d96
bernst4315 <bernst4315@gmail.com>
2025-02-26 15:42:09 -0700

    display drawn cards

--- src/App.jsx
+++ src/App.jsx
@@ -4,32 +4,40 @@ import './App.css'
 
 function App() {
   const deckId = "ot3zvj133gc4"
-  const [cardUrl, setCardUrl] = useState(`https://deckofcardsapi.com/api/deck/${deckId}/shuffle/?deck_count=1`)
+  const cardUrl = `https://deckofcardsapi.com/api/deck/${deckId}/shuffle/?deck_count=1`
   
-  useEffect(() => {
-    async function getDeck(){
-      const response = await fetch(cardUrl);
-      const data = await response.json();
-      console.log(data);
-      //return data; 
-    }
-  getDeck();
+  const [hand, setHand] = useState([])
 
-  })
+  useEffect(() => {
+    // async function getDeck(){
+    //   const response = await fetch(cardUrl);
+    //   const data = await response.json();
+    //   console.log(data);
+      //return data; 
+    //}
+  //getDeck();
+  drawCards(); 
+  }, [])
 
 async function drawCards(){
   const response = await fetch(`https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=2`);
   const data = await response.json();
   console.log(data)
+  setHand(data.cards)
+
 }
 
-drawCards()
-
+//let cards = drawCards();
+//console.log("you drew" + cards);
+console.log(hand)
+if(hand.length){
   return (
     <>
-  
+      {hand.map((card) => <img src={card.image} alt="" />)}
     </>
   )
+}
+
 }
 
 export default App
