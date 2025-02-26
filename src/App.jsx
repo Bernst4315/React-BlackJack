3985a3b7dbcc03a9cad390ddfd06d0ea4b739740
bernst4315 <bernst4315@gmail.com>
2025-02-26 15:06:15 -0700

    set up card api and set up draw function

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,33 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 
 import './App.css'
 
 function App() {
+  const deckId = "ot3zvj133gc4"
+  const [cardUrl, setCardUrl] = useState(`https://deckofcardsapi.com/api/deck/${deckId}/shuffle/?deck_count=1`)
+  
+  useEffect(() => {
+    async function getDeck(){
+      const response = await fetch(cardUrl);
+      const data = await response.json();
+      console.log(data);
+      //return data; 
+    }
+  getDeck();
+
+  })
+
+async function drawCards(){
+  const response = await fetch(`https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=2`);
+  const data = await response.json();
+  console.log(data)
+}
+
+drawCards()
 
   return (
     <>
-   
+  
     </>
   )
 }
