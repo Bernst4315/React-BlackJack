4c3b6c94b03d98aa48fef23b3cb013feac0b1776
bernst4315 <bernst4315@gmail.com>
2025-02-26 23:23:28 -0700

    successfully draws a card

--- src/App.jsx
+++ src/App.jsx
@@ -9,13 +9,14 @@ function App() {
   const [hand, setHand] = useState([])
 
   useEffect(() => {
-    // async function getDeck(){
-    //   const response = await fetch(cardUrl);
-    //   const data = await response.json();
-    //   console.log(data);
-      //return data; 
-    //}
-  //getDeck();
+    //get deck function resets the deck upon refresh
+    async function getDeck(){
+      const response = await fetch(cardUrl);
+      const data = await response.json();
+      console.log(data);
+      return data; 
+    }
+  getDeck();
   drawHand(); 
   }, [])
 
@@ -31,20 +32,17 @@ async function drawCard(){
   const response = await fetch(`https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=1`);
   const data = await response.json();
   console.log(data)
-  hand.push(data);
-  setHand(data.cards)
+  const card = data.cards;
+  setHand((prevHand) => [...prevHand, card[0]]);
 
 }
 
 function handleClick(){
-  console.log("click")
   drawCard()
 }
 
-//let cards = drawCards();
-//console.log("you drew" + cards);
-console.log(hand)
 if(hand.length){
+ 
   return (
     <>
       {hand.map((card) => <img src={card.image} alt="" />)}
