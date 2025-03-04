7889b1313aea8e4ec1f5ca9fe8000d5f0a8fc5a6
bernst4315 <bernst4315@gmail.com>
2025-03-04 06:43:32 -0700

    created lose condition

--- src/App.jsx
+++ src/App.jsx
@@ -1,115 +1,141 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useState } from "react";
 
-import './App.css'
+import "./App.css";
 
 function App() {
-  const deckId = "5atqk8u5sb4u"
-  const cardUrl = `https://deckofcardsapi.com/api/deck/${deckId}/shuffle/?deck_count=1`
+  const deckId = "5atqk8u5sb4u";
+  const cardUrl = `https://deckofcardsapi.com/api/deck/${deckId}/shuffle/?deck_count=1`;
   const handValue = [];
-  
+
   //let value = handValue.reduce((a,b) => a + b, 0)
   //console.log("the value is " + value);
-  const [hand, setHand] = useState([])
-  const [scoreMain, setScoreMain] = useState(0)
+  const [hand, setHand] = useState([]);
+  const [scoreMain, setScoreMain] = useState(0);
 
   useEffect(() => {
     //get deck function resets the deck upon refresh
-    async function getDeck(){
+    async function getDeck() {
       const response = await fetch(cardUrl);
       const data = await response.json();
       //console.log(data);
-      return data; 
+      return data;
     }
-  getDeck();
-  drawHand(); 
-  //getDeckId(); 
-  }, [])
+    getDeck();
+    drawHand();
+    //getDeckId();
+  }, []);
 
-//This function gets a deck ID from the API if the one provided doesn't work. Uncomment to obtain
-//a new id and change the variable deckId on line 6
+  //This function gets a deck ID from the API if the one provided doesn't work. Uncomment to obtain
+  //a new id and change the variable deckId on line 6
 
-// async function getDeckId() {
-//   const response = await fetch("https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1")
-//   const data = await response.json();
-//   console.log(data)
-//   const deckId = data.deck_id;
-//   return deckId; 
-// }
+  // async function getDeckId() {
+  //   const response = await fetch("https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1")
+  //   const data = await response.json();
+  //   console.log(data)
+  //   const deckId = data.deck_id;
+  //   return deckId;
+  // }
 
-async function drawHand(){
-  const response = await fetch(`https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=2`);
-  const data = await response.json();
-  console.log(data)
-  setHand(data.cards)
-  console.log(data.cards)
-  const hand = data.cards
-  let score = 0; 
-  hand.map((card) => {
-
-    console.log(card.value)
-      if(card.value === "JACK" || card.value === "QUEEN" || card.value === "King"){
-        //card.value = 10; 
-        score += 10
-      } else if(card.value === "ACE"){
-        score += 1; 
-      }else{
-        score += Number(card.value)
+  async function drawHand() {
+    const response = await fetch(
+      `https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=2`
+    );
+    const data = await response.json();
+    console.log(data);
+    setHand(data.cards);
+    console.log(data.cards);
+    const hand = data.cards;
+    let score = 0;
+    hand.map((card) => {
+      console.log(card.value);
+      if (
+        card.value === "JACK" ||
+        card.value === "QUEEN" ||
+        card.value === "King"
+      ) {
+        //card.value = 10;
+        score += 10;
+      } else if (card.value === "ACE") {
+        score += 1;
+      } else {
+        score += Number(card.value);
       }
 
       //score += card.value
-  })
+    });
 
-  setScoreMain(score)
-}
+    setScoreMain(score);
+  }
 
-async function drawCard(){
-  const response = await fetch(`https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=1`);
-  const data = await response.json();
-  //console.log(data)
-  const card = data.cards;
-  let score = scoreMain; 
-  
-  card.map((card) => {
+  async function drawCard() {
+    const response = await fetch(
+      `https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=1`
+    );
+    const data = await response.json();
+    //console.log(data)
+    const card = data.cards;
+    let score = scoreMain;
 
-    console.log(card.value)
-      if(card.value === "JACK" || card.value === "QUEEN" || card.value === "King"){
-        //card.value = 10; 
-        score += 10
-      } else if(card.value === "ACE"){
-        score += 1; 
-      }else{
-        score += Number(card.value)
+    card.map((card) => {
+      console.log(card.value);
+      if (
+        card.value === "JACK" ||
+        card.value === "QUEEN" ||
+        card.value === "KING"
+      ) {
+        //card.value = 10;
+        score += 10;
+      } else if (card.value === "ACE") {
+        score += 1;
+      } else {
+        score += Number(card.value);
       }
 
       //score += card.value
-  })
+    });
 
-  setScoreMain(score)
+    setScoreMain(score);
 
-  setHand([...hand, card[0]]);
+    setHand([...hand, card[0]]);
+  }
 
+  function handleClick() {
+    drawCard();
+  }
 
-}
-
-function handleClick(){
-  drawCard()
-}
-
-if(hand.length){
- 
-  return (
+  if (hand.length) {
+    if(scoreMain <= 21){
+    
+    return (
+      <>
+        {hand.map((card) => {
+          handValue.push(card.value);
+          //console.log(handValue);
+          return <img src={card.image} alt="" />;
+        })}
+        <p>{scoreMain}</p>
+        <button onClick={() => handleClick()}>Draw</button>
+        
+      </>
+    );
+}else{
+  return(
     <>
-      {hand.map((card) => {
-        handValue.push(card.value);
-        //console.log(handValue);
-        return <img src={card.image} alt="" />
-})}
-      <button onClick={() => handleClick()}>Draw</button>
-      <p>{scoreMain}</p>
+    {hand.map((card) => {
+      handValue.push(card.value);
+      //console.log(handValue);
+      return <img src={card.image} alt="" />;
+    })}
+    <p>{scoreMain}</p>
+    
+
+    <p>You Lose</p>
     </>
   )
 }
 
+
+  }
 }
 
-export default App
+export default App;
